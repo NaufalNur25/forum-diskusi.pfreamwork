99394ac0d2ddb9b0490e0f30240566506ff0d456
ibnu <ibnusyambodo2251@gmai.com>
2025-08-23 22:20:57 +0700

    posts modal

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>@yield('title', 'Website Sederhana')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         @vite('resources/css/app.css')
     </head>
     <body class="bg-gray-50 min-h-screen">
